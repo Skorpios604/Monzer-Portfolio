23e30f769be61305bc67fd8a6717c5f2abe323cb
Skorpios <cosmicray23@hotmail.com>
2025-12-15 14:49:28 -0800

    Did some editting on second card.

--- src/components/DisplayCardsDemo.tsx
+++ src/components/DisplayCardsDemo.tsx
@@ -17,12 +17,12 @@ const defaultCards = [
     {
         icon: <Sparkles className="size-4 text-blue-300" />,
         title: "AI Summit",
-        description: "Developed the official website for the 2nd Annual AI Summit in Vancouver, hosting 1000+ attendees.",
+        description: "🚀Developed the official website for the 2nd Annual AI 🤖 Summit in Vancouver, hosting 1000+ attendees.✨",
         date: "Upcoming",
         iconClassName: "text-blue-500",
         titleClassName: "text-gray-200",
         image: "/p26/ai-summit.png",
-        link: "https://aisummitvancouver.com",
+        link: "https://vanaisummit.com/",
         className:
             "[grid-area:stack] translate-x-12 translate-y-10 hover:-translate-y-64 before:absolute before:w-[100%] before:outline-1 before:rounded-xl before:outline-border before:h-[100%] before:content-[''] before:bg-blend-overlay before:bg-background/50 hover:before:opacity-0 before:transition-opacity before:duration-700 before:left-0 before:top-0 before:pointer-events-none",
     },
