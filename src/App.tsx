8aa4936a08110c4f72d8f792074977cb46093212
Skorpios <cosmicray23@hotmail.com>
2026-01-10 15:12:54 -0800

    Optimize gridscan for better mobile performance.

--- src/App.tsx
+++ src/App.tsx
@@ -94,7 +94,7 @@ function App() {
           gridScale={0.1}
           scanColor="#FF9FFC"
           scanOpacity={0.4}
-          enablePost
+          enablePost={false}
           bloomIntensity={0.6}
           chromaticAberration={0.002}
           noiseIntensity={0.01}
