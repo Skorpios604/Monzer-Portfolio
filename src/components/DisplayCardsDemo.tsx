cfeee8e507500d9ba22f44fec223a8b22359b0ff
Skorpios <cosmicray23@hotmail.com>
2026-01-09 17:27:00 -0800

    Remove hover effect from one of the cards.

--- src/components/DisplayCardsDemo.tsx
+++ src/components/DisplayCardsDemo.tsx
@@ -12,7 +12,7 @@ const defaultCards = [
 
         iconClassName: "text-blue-500",
         titleClassName: "text-gray-200",
-        className: "w-[350px] [grid-area:stack] hover:-translate-y-80 before:absolute before:w-[100%] before:outline-1 before:rounded-xl before:outline-border before:h-[100%] before:content-[''] before:bg-blend-overlay before:bg-background/50 hover:before:opacity-0 before:transition-opacity before:duration-700 before:left-0 before:top-0 before:pointer-events-none",
+        className: "w-[350px]",
         image: cardImage3,
         link: "https://skorpios604.github.io/E-commerce/",
     },
