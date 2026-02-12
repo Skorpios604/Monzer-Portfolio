fb4e10cf0a44442ba135eb714d6f5e14b1b163dc
Skorpios <cosmicray23@hotmail.com>
2026-02-12 15:54:54 -0800

    improve quality of icons.

--- src/components/ui/orbiting-skills.tsx
+++ src/components/ui/orbiting-skills.tsx
@@ -195,11 +195,11 @@ const OrbitingSkill = memo(({ config, angle }: OrbitingSkillProps) => {
 
   return (
     <div
-      className="absolute top-1/2 left-1/2 transition-all duration-300 ease-out"
+      className="absolute top-1/2 left-1/2 transition-all duration-300 ease-out will-change-transform"
       style={{
         width: `${size}px`,
         height: `${size}px`,
-        transform: `translate(calc(${x}px - 50%), calc(${y}px - 50%))`,
+        transform: `translate(calc(${x}px - 50%), calc(${y}px - 50%)) translateZ(0)`,
         zIndex: isHovered ? 20 : 10,
       }}
       onMouseEnter={() => setIsHovered(true)}
@@ -207,7 +207,7 @@ const OrbitingSkill = memo(({ config, angle }: OrbitingSkillProps) => {
     >
       <div
         className={`
-          relative w-full h-full p-2 bg-gray-800/90 backdrop-blur-sm
+          relative w-full h-full p-1.5 bg-gray-800/90 backdrop-blur-sm
           rounded-full flex items-center justify-center
           transition-all duration-300 cursor-pointer
           ${isHovered ? 'scale-125 shadow-2xl' : 'shadow-lg hover:shadow-xl'}
@@ -215,7 +215,10 @@ const OrbitingSkill = memo(({ config, angle }: OrbitingSkillProps) => {
         style={{
           boxShadow: isHovered
             ? `0 0 30px ${iconComponents[iconType]?.color}40, 0 0 60px ${iconComponents[iconType]?.color}20`
-            : undefined
+            : undefined,
+          backfaceVisibility: 'hidden',
+          WebkitFontSmoothing: 'antialiased',
+          transform: 'translateZ(0)',
         }}
       >
         <SkillIcon type={iconType} />
