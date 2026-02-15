ca3af4c43a4071696d2dbfcb52a416d12e8a8df5
Skorpios <cosmicray23@hotmail.com>
2026-02-15 10:00:02 -0800

    Change npm icon text size.

--- src/components/ui/orbiting-skills.tsx
+++ src/components/ui/orbiting-skills.tsx
@@ -114,7 +114,11 @@ const iconComponents: Record<IconType, { component: () => React.JSX.Element; col
     component: () => (
       <svg role="img" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="w-full h-full">
         <rect width="24" height="24" fill="#CB3837" rx="2" ry="2" />
-        <path fill="#FFF" transform="translate(3, 8)" d="M1,1v4h2v-3h1v3h1v-4h1v5h2v-4h1v2h-1v1h2v-4h1v4h2v-3h1v3h1v-3h1v3h1v-4" />
+        <path
+          fill="#FFF"
+          transform="translate(1.2, 7.8) scale(1.24)"
+          d="M1,1v4h2v-3h1v3h1v-4h1v5h2v-4h1v2h-1v1h2v-4h1v4h2v-3h1v3h1v-3h1v3h1v-4"
+        />
       </svg>
     ),
     color: '#CB3837'
